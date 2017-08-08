7d122e38f9908136071bbbd39ace7f6f0ebd7952
Marcus Ataide <marckaraujo86@gmail.com>
2017-08-08 19:12:50 -0300

    Add sitemap

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL="https://raj4qqa51e.execute-api.us-east-2.amazonaws.com/dev/beta";
+export const SERVER_URL="https://ynv9571wi7.execute-api.us-east-2.amazonaws.com/dev/beta";
 export const ANIMATION_OPTIONS = {offsetY: 0.6,  fullElementInView: false};

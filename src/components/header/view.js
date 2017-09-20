df476bab1bb61ca956053ddf8d3b8c470d1fa42e
Marcus Ataide <marckaraujo86@gmail.com>
2017-09-20 17:43:39 -0300

    Update Social Media

--- src/components/header/view.js
+++ src/components/header/view.js
@@ -26,9 +26,9 @@ var view = function(){
                         {/*<NavItem eventKey={2} active = {this.isActive("/faq")} href="/faq" className="text">{translate("header.faq")}</NavItem>*/}
 						<NavItem eventKey={3} href="#"><button onClick={()=>{this.props.openModal?this.props.openModal():null}} className="brand-button button"><span className="brand-button-text">{translate("header.signupBtn")}</span></button></NavItem>
 
-						<NavItem  target="_blank" href="https://www.facebook.com/emasters.co/"><img src={fb} alt="" className="social fb"/></NavItem>
-						<NavItem target="_blank"  href="https://www.instagram.com/emasters.co/"><img src={tw} alt="" className="social"/></NavItem>
-						<NavItem  target="_blank" href="https://twitter.com/eMastersco"><img src={insta} alt="" className="social"/></NavItem>
+						<NavItem  target="_blank" href="https://www.facebook.com/emastersapp/"><img src={fb} alt="" className="social fb"/></NavItem>
+						<NavItem target="_blank"  href="https://www.instagram.com/emastersapp/"><img src={tw} alt="" className="social"/></NavItem>
+						<NavItem  target="_blank" href="https://twitter.com/emastersapp"><img src={insta} alt="" className="social"/></NavItem>
 						<NavItem  target="_blank" href="https://www.youtube.com/channel/UCrQsnAmn33j6MWQiRVtpyzA"><img src={youtube} alt="" className="social youtube"/></NavItem>
 					</Nav>
 				</Navbar.Collapse>

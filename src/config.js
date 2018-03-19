38e66bff03fc59fffc89450dcf58dd6f82bfaa93
marcus-PC\marcus <marckaraujo86@gmail.com>
2018-03-19 14:28:43 -0300

    Add .htacess redirect

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL="https://ynv9571wi7.execute-api.us-east-2.amazonaws.com/dev/beta";
+export const SERVER_URL="https://2jdzbiazhj.execute-api.us-east-2.amazonaws.com/v1/beta";
 export const ANIMATION_OPTIONS = {offsetY: 0.6,  fullElementInView: false};

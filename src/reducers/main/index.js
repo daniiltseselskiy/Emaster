9ddbafd013c5a6389776eb6a090327b8fcdf1513
robbieve <robbieeve751994@gmail.com>
2018-08-23 01:32:59 +0800

    set localstorage for language and url

--- src/reducers/main/index.js
+++ src/reducers/main/index.js
@@ -5,16 +5,19 @@ import {
 
 import { labels_en, labels_cn, labels_pt} from '../../constants/labels'
 import content from '../../constants/content'
-
+const labels = {
+    "/pt": labels_pt,
+    "/en": labels_en,
+    "/cn": labels_cn,
+}
 const initialState = {
-   labels: labels_en,
+   labels: labels[localStorage.getItem('base')],
    content: content,
    base: localStorage.getItem('base') || "/en",
 };
 
 const reducer = (state = initialState, action) => {
     let newState = _.cloneDeep(state);
-    
     switch(action.type) {
         
         case CHANGE_LANGUAGE:
